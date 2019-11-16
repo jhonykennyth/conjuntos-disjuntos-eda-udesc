cb3ecfc736608399e92a245e9d80f8b7d561d3f8
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2019-11-16 14:21:48 -0300

    Iniciado desenvolvimento do TAD

--- conjuntos.h
+++ conjuntos.h
@@ -1,1 +1,23 @@
+#define ERRO_CONJUNTO_VAZIO -1
 
+typedef struct ele
+{
+    void *info;
+    struct ele *proximo;//ultimo elemento aponta para null
+} Elemento;
+
+typedef struct
+{
+    Elemento *cabeca;
+    int tamInfo, qtd;
+} Lista;
+
+typedef struct
+{
+    Lista multi;
+    int tamInfo;
+} Conjuntos;
+
+void inicializaConjunto(Conjuntos *c, int t){
+
+}
